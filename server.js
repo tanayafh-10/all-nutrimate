76b69d85771a469b103e1b7c3302ec79e030229e
tanayafh-10 <128033228+tanayafh-10@users.noreply.github.com>
2024-12-09 22:14:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 (async () => {
     try {
         const server = Hapi.server({
-            port: process.env.PORT || 3000,
+            port: process.env.PORT || 8080,
             host: '0.0.0.0',
             routes: {
                 cors: {
