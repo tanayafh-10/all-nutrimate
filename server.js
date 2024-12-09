9015267774d9a2f47898114dfa9e112293c27a71
tanayafh-10 <128033228+tanayafh-10@users.noreply.github.com>
2024-12-09 17:11:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./src/routes/routes');
 require('dotenv').config();
 
+(async () => {
+    try {
+        const server = Hapi.server({
+            port: process.env.PORT || 3000,
+            host: '0.0.0.0',
+            routes: {
+                cors: {
+                    origin: ['*'], // Mengizinkan semua domain
+                },
+            },
+        });
 
+        // Daftarkan semua route ke server
+        routes(server);
 
-const init = async () => {
-    const server = Hapi.server({
-        port: 3000,
-        host: '0.0.0.0',
-    });
-
-    // Daftarkan semua route ke server
-    routes(server);
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
-};
-
-init().catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
+        // Mulai server
+        await server.start();
+        console.log('Server running on %s', server.info.uri);
+    } catch (err) {
+        console.error('Error starting server:', err);
+        process.exit(1);
+    }
+})();
